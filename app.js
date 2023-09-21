3bd75ad12ab72645616dc9bef1ef00cd9bdaeca8
ConnorLBrown <cbrown30@una.edu>
2023-09-21 16:14:11 -0500

    Update app.js
    URI > uri

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(URI, {
+const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
     strict: true,
